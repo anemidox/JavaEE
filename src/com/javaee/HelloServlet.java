84a95ede03f41e7a0cdd8bb7edc6d99a4e096926
animidox <dhanujadisanayke@gmail.com>
2025-03-08 02:28:39 +0530

    change

--- src/com/javaee/HelloServlet.java
+++ src/com/javaee/HelloServlet.java
@@ -1,15 +1,28 @@
 package com.javaee;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
 
-@WebServlet("/hello")
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().write("Hello, World!");
+    private String message;
+
+    public void init() {
+        message = "Good Bye World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
     }
 }
