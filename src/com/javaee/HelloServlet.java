451a64ebf7b6490c00c0648a432545058057c29d
animidox <dhanujadisanayke@gmail.com>
2025-03-08 23:02:50 +0530

    delete main class

--- src/com/javaee/HelloServlet.java
+++ src/com/javaee/HelloServlet.java
@@ -1,28 +1,15 @@
 package com.javaee;
 
-import java.io.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
-
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Good Bye World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().write("Hello, World!");
     }
 }
