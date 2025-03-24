b3575bacccf30b768994da924906ca32a6d6719a
animidox <dhanujadisanayke@gmail.com>
2025-03-24 20:03:52 +0530

    basic working code

---
+++ src/com/mycompany/servlets/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.mycompany.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Welcome to Java EE Web Application Test</h1>");
+    }
+}
