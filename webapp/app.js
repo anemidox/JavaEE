a169700a26c83221524734402742af77adbf3de5
animidox <dhanujadisanayke@gmail.com>
2025-03-28 13:00:39 +0530

    workig code on web components

---
+++ webapp/app.js
@@ -0,0 +1,10 @@
+const app = () => {
+    return `
+        <div>
+            <h1>My Web App</h1>
+            <p>This is a simple web application.</p>
+        </div>
+    `;
+}
+
+export default app;
